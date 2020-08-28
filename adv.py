125c622e2773bf0a7735a4b7ac51d3717ca18474
abrobins <abrgroup@gmail.com>
2020-08-27 21:28:37 -0400

    added basic dft and bfs functions

--- adv.py
+++ adv.py
@@ -40,6 +40,61 @@ class Stack():
         return len(self.stack)
 
 
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+
+
+def dft(self, starting_vertex):
+    visited = set()
+    # create a stack and add starting_vertex
+    s = stack()
+    s.push([starting_vertex])
+    while s.size() > 0:
+        # pop the last vertex that was added
+        curr_node = s.pop()
+        room = curr_node[-1]
+
+        # next we need to see if room has been visited
+        if room not in visited:
+            # set vertices based  on id of room
+            self.vertices[room.id] = {}
+            # add connected room and direction
+            for potential_dir in room.get_exits():
+                self.vertices[room.id][room.get_room_in_direction(
+                    potential_dir).id] = potential_dir
+            # mark this room as visited
+            visited.add(room)
+
+            exit_options = room.get_exits()
+            while len(exit_options) > 0:
+                room_direction = exit_options[0]
+                # create neighbor path
+                curr_path = list(curr_node)
+                curr_path.append(room.get_room_in_direction(direction))
+                # add to stack
+                stack.push(neighbor_path)
+                # remove to go to next direction option
+                exits.remove(room_direction)
+    return self.vertices
+
+
+def bfs(self, starting_vertex, destination_vertex):
+    q = Queue()
+    q.enqueue([starting_vertex])
+    visited = set()
+    while q.size() > 0:
+        curr_path = q.dequeue()
+        last_vert = curr_path[-1]
+        if last_vert == destination_vertex:
+            return curr_path
+        visited.add(last_vert)
+        for room_id in self.vertices[last_vertex].keys():
+            path_of_neighbors = list(curr_path)
+            path_of_neighbors.append(room_id)
+            q.enqueue(path_of_neighbors)
+
+
 # Load world
 world = World()
 
@@ -85,12 +140,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
