5727ef420fba2edd75cf450ea7bd0570bcb41554
abrobins <abrgroup@gmail.com>
2020-08-30 19:22:27 -0400

    working on while loop to tie DFT and BFS together

--- adv.py
+++ adv.py
@@ -44,55 +44,55 @@ class Graph:
     def __init__(self):
         self.vertices = {}
 
+    def dft(self, starting_vertex):
+        visited = set()
+        # create a stack and add starting_vertex
+        s = Stack()
+        s.push([starting_vertex])
+        while s.size() > 0:
+            # pop the last vertex that was added
+            curr_node = s.pop()
+            room = curr_node[-1]
 
-def dft(self, starting_vertex):
-    visited = set()
-    # create a stack and add starting_vertex
-    s = stack()
-    s.push([starting_vertex])
-    while s.size() > 0:
-        # pop the last vertex that was added
-        curr_node = s.pop()
-        room = curr_node[-1]
+            # next we need to see if room has been visited
+            if room not in visited:
+                # set vertices based  on id of room
+                self.vertices[room.id] = {}
+                # add connected room and direction
+                for potential_dir in room.get_exits():
+                    self.vertices[room.id][room.get_room_in_direction(
+                        potential_dir).id] = potential_dir
+                # mark this room as visited
+                visited.add(room)
 
-        # next we need to see if room has been visited
-        if room not in visited:
-            # set vertices based  on id of room
-            self.vertices[room.id] = {}
-            # add connected room and direction
-            for potential_dir in room.get_exits():
-                self.vertices[room.id][room.get_room_in_direction(
-                    potential_dir).id] = potential_dir
-            # mark this room as visited
-            visited.add(room)
+                exit_options = room.get_exits()
+                while len(exit_options) > 0:
+                    room_direction = exit_options[0]
+                    # create neighbor path
+                    neighbor_path = list(curr_node)
+                    neighbor_path.append(
+                        room.get_room_in_direction(room_direction))
+                    # add to stack
+                    s.push(neighbor_path)
+                    # remove to go to next direction option
+                    exit_options.remove(room_direction)
+        return self.vertices
 
-            exit_options = room.get_exits()
-            while len(exit_options) > 0:
-                room_direction = exit_options[0]
-                # create neighbor path
-                curr_path = list(curr_node)
-                curr_path.append(room.get_room_in_direction(direction))
-                # add to stack
-                stack.push(neighbor_path)
-                # remove to go to next direction option
-                exits.remove(room_direction)
-    return self.vertices
-
-
-def bfs(self, starting_vertex, destination_vertex):
-    q = Queue()
-    q.enqueue([starting_vertex])
-    visited = set()
-    while q.size() > 0:
-        curr_path = q.dequeue()
-        last_vert = curr_path[-1]
-        if last_vert == destination_vertex:
-            return curr_path
-        visited.add(last_vert)
-        for room_id in self.vertices[last_vertex].keys():
-            path_of_neighbors = list(curr_path)
-            path_of_neighbors.append(room_id)
-            q.enqueue(path_of_neighbors)
+    def bfs(self, starting_vertex, destination_vertex):
+        q = Queue()
+        q.enqueue([starting_vertex])
+        visited = set()
+        while q.size() > 0:
+            curr_path = q.dequeue()
+            last_vert = curr_path[-1]
+            if last_vert not in visited:
+                if last_vert == destination_vertex:
+                    return curr_path
+                visited.add(last_vert)
+                for room_id in self.vertices[last_vert].keys():
+                    path_of_neighbors = list(curr_path)
+                    path_of_neighbors.append(room_id)
+                    q.enqueue(path_of_neighbors)
 
 
 # Load world
@@ -114,6 +114,16 @@ world.load_graph(room_graph)
 world.print_rooms()
 
 player = Player(world.starting_room)
+
+# Fill this out with directions to walk
+# traversal_path = ['n', 'n']
+
+graph = Graph()
+# use DFT function to create a graphed map of the 'world'
+rooms_dft = graph.dft(player.current_room)
+# Extract all room id's into a list
+rooms_list_dft = [room_id for room_id in rooms_dft.keys()]
+
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
